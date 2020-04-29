dc7364e33150d102ea98cf6e3ca2a57954174485
Keenen Leyson <keenen.leyson@gmail.com>
2020-04-30 11:40:43 +1200

    added updateCounts function

--- game.js
+++ game.js
@@ -3,6 +3,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
+
 function start () {
   bindEventListeners(document.getElementsByClassName('board')[0].children)
 }
@@ -12,18 +13,36 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
 function makeGreen (evt) {
   evt.preventDefault()
-  evt.target.classList.toggle('green')
+  evt.target.classList.toggle('blue', false) //removes the blue
+  evt.target.classList.toggle('invisible', false) //removes the white
+  evt.target.classList.toggle('green') //adds the green
   updateCounts()
 }
 
 // CREATE FUNCTION makeBlue HERE
+function makeBlue(evt) {
+  evt.preventDefault()
+  evt.target.classList.toggle('green', false) //removes the green
+  evt.target.classList.toggle('invisible', false) //removes the white
+  evt.target.classList.toggle('blue') //adds the blue
+  updateCounts()
+}
 
 // CREATE FUNCTION hide HERE
+function hide(evt) {
+  evt.preventDefault()
+  evt.target.classList.toggle('green', false) //removes the green
+  evt.target.classList.toggle('blue', false) //removes the blue
+  evt.target.classList.toggle('invisible') //adds the white
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
@@ -31,9 +50,17 @@ function updateCounts () {
     green: 0,
     invisible: 0
   }
-  
+
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
 
+  var blueCount = document.getElementsByClassName("blue").length;
+  var greenCount = document.getElementsByClassName("green").length;
+  var invisibleCount = document.getElementsByClassName("invisible").length;
+
+  totals.blue = blueCount
+  totals.green = greenCount
+  totals.invisible = invisibleCount
+  
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
